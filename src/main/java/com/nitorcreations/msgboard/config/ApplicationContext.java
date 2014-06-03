fc41e59f383832ff97699da766c02e64ae06c5a3
eero <eero.norri@nitorcreations.com>
2014-06-03 19:24:51 +0300

    Format

--- src/main/java/com/nitorcreations/msgboard/config/ApplicationContext.java
+++ src/main/java/com/nitorcreations/msgboard/config/ApplicationContext.java
@@ -5,4 +5,5 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = { "com.nitorcreations.msgboard" })
-public class ApplicationContext {}
+public class ApplicationContext {
+}
